f4a88a3dad766f501e88caba64f600095aa84fb6
Long G <long.w.guan@gmail.com>
2023-03-16 22:34:34 -0700

    Updated layout and styling to match project

--- app_home.js
+++ app_home.js
@@ -1,3 +1,7 @@
 document.querySelector('.knight').addEventListener('click', ()=> {
     window.location = "/classic-games/knight-travails/index_kt.html";
 });
+
+document.querySelector('.ttt').addEventListener('click', ()=> {
+    window.location = "/classic-games/tic-tac-toe/index_tic-tac-toe.html";
+});
