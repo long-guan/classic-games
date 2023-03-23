7307124a1f25bfc1841192542885a37eefd2a1dc
Long G <long.w.guan@gmail.com>
2023-03-23 12:35:20 -0700

    Added function to place Select Start marker and Select End marker

--- lazy-knight/app_kt.js
+++ lazy-knight/app_kt.js
@@ -1,13 +1,15 @@
 import {buildTree} from '/classic-games/lazy-knight/buildTree.js';
 import {getMoves} from '/classic-games/lazy-knight/traverseTree_kt.js';
-import {selectStart} from '/classic-games/lazy-knight/select.js';
+import {selectStart, selectEnd} from '/classic-games/lazy-knight/select.js';
+
 
 // add functionality for Select Start btn
 document.querySelector('.start').addEventListener('click', selectStart);
 
+// add functionality of Select End btn
+document.querySelector('.end').addEventListener('click', selectEnd);
+
 knightMoves([0,0], [7,7]);
-
-
 
 // builds the tree of possible moves and returns the possible moves
 function knightMoves(start, end) {
--- lazy-knight/select.js
+++ lazy-knight/select.js
@@ -1,10 +1,13 @@
-let startBtnStatus = false;
+let startBtnStatus = false; // true when Select Start btn is selected
+let endBtnStatus = false; // true when Select End btn is selected
 
+let selectStartBtn = document.querySelector('.start');
+let selectEndBtn = document.querySelector('.end');
 
 // add event listeners to all squares
 let squares = document.querySelectorAll('.board-cont>div');
 for (let square of squares) {
-    square.addEventListener('click', addKnight);
+    square.addEventListener('click', addMarker);
 }
 
 // updates status of startBtnStatus and updates interface for Select Start btn
@@ -12,44 +15,65 @@ export function selectStart() {
     btnStatus(this);
 }
 
-// if startBtnStatus is true, add knight to the selected square
-function addKnight() {
-    if (startBtnStatus === true) {
-        removeExKnight();
-        this.appendChild(createKnightImg());
-    }
-}
-
-// removes any existing knights on board
-function removeExKnight() {
-    for (let square of squares) {
-        if (square.hasChildNodes()) {
-            square.innerHTML = '';
-        }
-    }
-}
-
-// returns knight SVG element
-function createKnightImg() {
-    let img = document.createElement("img");
-    img.src = '/classic-games/image/chess-knight.svg';
-    img.classList.add('knight');
-    return img;
+export function selectEnd() {
+    btnStatus(this)
 }
 
 // add bolder and larger font and change background color of btn to show selection
-// updates startBtnStatus to true or false
+// updates Select Start and Select End button status to true or false
 function btnStatus(btn) {
-    // console.log(btn);
-    if (btn.classList.contains('selected')) {
+    if (btn.classList.contains('selected')) { // remove styling
         btn.classList.remove('selected');
-        if (btn.classList.contains('start')) {
-            startBtnStatus = false;
-        }
-    } else {
+        startBtnStatus = false;
+        endBtnStatus = false;
+    } else { // add styling
         btn.classList.add('selected');
         if (btn.classList.contains('start')) {
             startBtnStatus = true;
+            selectEndBtn.classList.remove('selected');
+            endBtnStatus = false;
+        } else if (btn.classList.contains('end')) {
+            endBtnStatus = true;
+            selectStartBtn.classList.remove('selected');
+            startBtnStatus = false;
         }
     }
+}
+
+// if status is true, add markers to the selected square
+function addMarker() {
+    if (this.hasChildNodes() === false) { // prevents markers being added to squares with markers already
+        if (startBtnStatus === true) {
+            removeExMarkers();
+            this.appendChild(createImg('/classic-games/image/chess-knight.svg', 'knight'));
+        } else if (endBtnStatus === true) {
+            removeExMarkers();
+            this.appendChild(createImg('/classic-games/image/map-marker.svg', 'end-marker'))
+        }
+    }
+}
+
+// removes any existing marker on board for new selection
+function removeExMarkers() {
+    for (let square of squares) {
+        if (square.hasChildNodes()) { // check if it has child nodes
+            if (startBtnStatus === true) { // remove knight marker
+                if (square.firstChild.classList.contains('knight')) {
+                    square.innerHTML = '';
+                }
+            } else if (endBtnStatus === true) { // remove end marker
+                if (square.firstChild.classList.contains('end-marker')) {
+                    square.innerHTML = '';
+                }
+            }
+        }
+    }
+}
+
+// creates image with given source and class
+function createImg(src, classList) {
+    let img = document.createElement("img");
+    img.src = src;
+    img.classList.add(classList);
+    return img;
 }
