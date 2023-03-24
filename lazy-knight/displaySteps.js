586a95acb09fe69d9867da1d1853c4591ec740ae
Long G <long.w.guan@gmail.com>
2023-03-24 13:26:13 -0700

    Added delay to show each move on the board

--- lazy-knight/displaySteps.js
+++ lazy-knight/displaySteps.js
@@ -4,16 +4,23 @@ export function displaySteps(moves) {
     console.log(moves);
     let stepCount = 0;
     for (let move of moves) {
-        if (stepCount === 0) {
-            document.getElementById(move[0] + '' + move[1]).appendChild(createStepMarker('Start'));
-            stepCount++;
-        } else {
-            document.getElementById(move[0] + '' + move[1]).appendChild(createStepMarker(stepCount));
-            stepCount++;
-        }
+        addMarker(move, stepCount);
+        stepCount++;
+        console.log(stepCount);
     }
 }
 
+function addMarker(move, stepCount) {
+    setTimeout(() => {
+        if (stepCount === 0) {
+            document.getElementById(move[0] + '' + move[1]).appendChild(createStepMarker('Start'));
+        } else {
+            document.getElementById(move[0] + '' + move[1]).appendChild(createStepMarker(stepCount));
+        }
+    }, stepCount * 750);
+}
+
+// return a div marker for the board
 function createStepMarker(markerText) {
     let div = document.createElement('div');
     div.classList.add('step-marker');
