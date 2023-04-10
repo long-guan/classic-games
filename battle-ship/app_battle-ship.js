42cd43d43a55a58f2660a17db78930e57bd561da
Long G <long.w.guan@gmail.com>
2023-04-10 15:45:38 -0700

    Added createBoard.js to create board and title

--- battle-ship/app_battle-ship.js
+++ battle-ship/app_battle-ship.js
@@ -1,1 +1,5 @@
 import {gameboard} from '/classic-games/battle-ship/gameboard.js';
+import {createBoard} from '/classic-games/battle-ship/createBoard.js';
+
+createBoard('player1-board', 'Friendly Waters');
+createBoard('computer-board', 'Enemy Waters');
---
+++ battle-ship/createBoard.js
@@ -0,0 +1,41 @@
+export function createBoard(contName, titleName) {
+    createSubBoardCont(contName);
+    createTitle(contName, titleName);
+    createSquareCont(contName);
+    createSquares(contName)
+}
+
+// creates a sub-container for a player or computer
+function createSubBoardCont(contName) {
+    let boardCont = document.querySelector('.board-cont');
+    let subBoardCont = document.createElement('div');
+    subBoardCont.classList.add(`${contName}`);
+    boardCont.appendChild(subBoardCont);
+}
+
+// creates square board container and adds it to the sub-container
+function createSquareCont(contName) {
+    let subBoardCont = document.querySelector('.' + contName);
+    let div = document.createElement('div');
+    div.classList.add('board-style');
+    div.classList.add(`${contName}-square`);
+    subBoardCont.appendChild(div);
+}
+
+// creates squares with ID corresponding to its coordinate
+function createSquares(contName) {
+    let subBordCont = document.querySelector('.' + contName + '-square');
+    for (let i = 0; i < 100; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('id', `${i}`);
+        div.classList.add('square');
+        subBordCont.appendChild(div);
+    }
+}
+
+function createTitle(contName, titleName) {
+    let newTitle = document.createElement('h3');
+    newTitle.innerHTML = `${titleName}`;
+    let subBoardCont = document.querySelector('.' + contName);
+    subBoardCont.appendChild(newTitle);
+}
