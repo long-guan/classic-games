a6f02d55f301062244d19767252c5b78d0ad579a
Long G <long.w.guan@gmail.com>
2023-04-27 15:44:50 -0700

    Added addClickForAttack and playerAttack for player to choose square to attack on enemy board

--- battle-ship/attack-phase-loop.js
+++ battle-ship/attack-phase-loop.js
@@ -1,5 +1,7 @@
 import {createBoard} from '/classic-games/battle-ship/createBoard.js';
 import { removeBoardContEle } from '/classic-games/battle-ship/start-menu-select.js';
+import { displayShipPlacement } from '/classic-games/battle-ship/display-new-move.js';
+import { player1Board, computerBoard } from '/classic-games/battle-ship/gameboard.js';
 
 export function attackPhaseLoop() {
     setUpAttackPhase();
@@ -8,19 +10,38 @@ export function attackPhaseLoop() {
 // removes board for placing ships
 // creates board for player and computer
 // updates board-cont flex direction to be row
+// display friendly ship locations on Friendly Water board
 function setUpAttackPhase() {
     removeBoardContEle();
     createBoard('player', 'Friendly Water');
     createBoard('computer', 'Enemy Water');
     document.querySelector('.board-cont').style.flexDirection = 'row';
     removeFriendlyHover();
+    displayShipPlacement(player1Board);
+    addClickForAttack();
 }
 
 // remove hover from friendly water board
 function removeFriendlyHover() {
     let friendlySquares = document.querySelectorAll('.player-square>div');
-    console.log(friendlySquares);
     Array.from(friendlySquares).forEach(square => {
         square.classList.remove('hover');
     });
+}
+
+function addClickForAttack() {
+    let enemySquares = document.querySelectorAll('.computer-square>div');
+    Array.from(enemySquares).forEach(square => {
+        square.addEventListener('click', playerAttack, {once: true});
+    });
+}
+
+// updates color to be red if hit and gray if miss
+// updates computerBoard.position
+function playerAttack() {
+    if (computerBoard.receiveAttack(this.id)) {
+        this.style.backgroundColor = 'red';
+    } else {
+        this.style.backgroundColor = 'gray';
+    }
 }
