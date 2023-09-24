e30b8847ae12ce0baeded042b57a595f05cb1a27
Long G <long.w.guan@gmail.com>
2023-09-23 18:28:38 -0700

    Fixing navlinks

--- navbar.js
+++ navbar.js
@@ -1,15 +1,15 @@
-// Home button
-document.querySelector('.home-btn').addEventListener('click', ()=> {
+// // Home button
+// document.querySelector('.home-btn').addEventListener('click', ()=> {
 
-    window.location = "/index.html";
-});
+//     window.location = "/index.html";
+// });
 
-// Logo and Title
-document.querySelector('.logo-title').addEventListener('click', ()=> {
-    window.location = "/index.html";
-});
+// // Logo and Title
+// document.querySelector('.logo-title').addEventListener('click', ()=> {
+//     window.location = "/index.html";
+// });
 
-// About
-document.querySelector('.about').addEventListener('click', () => {
-    window.location = "/about-page/index_about.html";
-})
+// // About
+// document.querySelector('.about').addEventListener('click', () => {
+//     window.location = "/about-page/index_about.html";
+// })
